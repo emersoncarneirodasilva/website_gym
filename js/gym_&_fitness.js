1c5d7f1edb8884913e68b1959728431e7af4ca30
Emerson Carneiro da Silva <94311606+emersoncarneirodasilva@users.noreply.github.com>
2022-02-23 00:33:50 -0300

    Up

---
+++ js/gym_&_fitness.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    let menuToggle = document.querySelector(".toggle")
+    let banner = document.querySelector(".banner")
+
+    menuToggle.classList.toggle("active")
+    banner.classList.toggle("active")
+}
